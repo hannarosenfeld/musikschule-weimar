bb12e5355902d32f88341a29459e9316c3af1db9
hannarosenfeld <hannazrosenfeld@gmail.com>
2022-01-31 21:59:05 -0500

    Test

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,34 +7,39 @@ import youtube_logo from "../images/Header_07.png"
 
 const NavBar = () => {
     return(
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	    <a class="navbar-brand" href="#">Navbar</a>
+	<nav class="header-navbar navbar navbar-expand-lg navbar-light bg-light">
+
 	    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	    </button>
 
 	    <div class="collapse navbar-collapse" id="navbarSupportedContent">
 		<ul class="navbar-nav mr-auto">
+		    <li style={{width: "30%"}}>
+			<img class="navbar-brand" src={logo} />
+		    </li>
 		    <li class="nav-item active">
-			<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+			<a class="nav-link" href="#">Ueber Uns</a>
 		    </li>
 		    <li class="nav-item">
-			<a class="nav-link" href="#">Link</a>
-		    </li>
-		    <li class="nav-item dropdown">
-			<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-			    Dropdown
-			</a>
-			<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-			    <a class="dropdown-item" href="#">Action</a>
-			    <a class="dropdown-item" href="#">Another action</a>
-			    <div class="dropdown-divider"></div>
-			    <a class="dropdown-item" href="#">Something else here</a>
-			</div>
+			<a class="nav-link" href="#">Instrumente</a>
 		    </li>
 		    <li class="nav-item">
-			<a class="nav-link disabled" href="#">Disabled</a>
+			<a class="nav-link" href="#">Projekte</a>
 		    </li>
+		    <li class="nav-item">
+			<a class="nav-link" href="#">Unterstuetzung</a>
+		    </li>
+		    <li class="nav-item">
+			<a class="nav-link" href="#">Service</a>
+		    </li>
+		    <li class="nav-item">
+			<a class="nav-link" href="#">Anmeldung</a>
+		    </li>
+			<li style={{width: "30%"}}>			
+			    <img class="navbar-brand" src={youtube_logo} />
+			</li>
+			
 		</ul>
 	    </div>
 	</nav>
