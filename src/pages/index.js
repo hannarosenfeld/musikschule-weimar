b5dfaefbab371e4b547512d6f31044dfb530e725
hannarosenfeld <hannazrosenfeld@gmail.com>
2022-01-31 19:34:02 -0500

    Test

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,12 @@ import React from "react"
 import Layout from "../components/Layout"
 import DropDownRows from "../components/DropDownRows"
 
+import background from "../images/HG_Element_01.png"
 
 const IndexPage = ({ data }) => {
     return (
-	<div>
 	    <Layout>
-		<div className="willkommen" width="300">
-		</div>
+		<img style={{margin: "auto 2em"}} className="willkommen container" src={background} />
 		<section className="paedagogen-und-verwaltung mt-5">
 		    <div className="container d-flex flex-column">
 			<h1>PÄDAGOGEN & VERWALTUNG</h1>
@@ -17,7 +16,6 @@ const IndexPage = ({ data }) => {
 		    </div>
 		</section>
 	    </Layout>
-	</div>
     )
 }
 
